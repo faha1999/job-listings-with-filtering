661f1fce328ea11c9cd23aff1d36e6b058db1cb7
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-10-05 20:07:57 +0600

    add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './themes/themes';
+import useTheme from './themes/UseTheme';
 
 export const App = () => {
-  return <div>App</div>;
+  const [theme, toggleTheme] = useTheme();
+  return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      App
+    </ThemeProvider>
+  );
 };
