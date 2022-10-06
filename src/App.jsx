ea64aee382883b53fa3e7771cc816100a71b60a4
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-10-06 23:34:13 +0600

    add jobcard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { JobCard } from './components/jobcard/JobCard';
 import { darkTheme, lightTheme } from './themes/themes';
 import useTheme from './themes/UseTheme';
 
@@ -7,7 +8,7 @@ export const App = () => {
   const [theme, toggleTheme] = useTheme();
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      App
+      <JobCard />
     </ThemeProvider>
   );
 };
---
+++ src/components/jobcard/JobCard.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const JobCard = () => {
+  return <div>JobCard</div>;
+};
