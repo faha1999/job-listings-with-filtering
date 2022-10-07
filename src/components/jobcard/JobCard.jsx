5185d0e8d84ece4bff8f9cd6bd93496124aa070c
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-10-08 00:17:50 +0600

    added JobCard

--- src/components/jobcard/JobCard.jsx
+++ src/components/jobcard/JobCard.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
+import { Context } from '../../Context';
 
 const Wrapper = styled.div`
   background: ${({ theme }) => theme.cardBg};
@@ -22,5 +23,57 @@ const Skill = styled.button`
   }
 `;
 export const JobCard = () => {
-  return <div>JobCard</div>;
+  const { filters, addFilter } = useContext(Context);
+  const skills = [job.role, job.level, ...job.languages, ...job.tools];
+
+  return (
+    <Wrapper className="Wrapper">
+      <div className="Column">
+        <img
+          className="CompanyLogo"
+          src={job.logo}
+          alt={`${job.company} Logo`}
+        />
+
+        <div className="CompanyInfoWrapper">
+          {job.new && (
+            <span className="NewJob" onClick={() => addFilter('New')}>
+              New!
+            </span>
+          )}
+          {job.featured && (
+            <span className="FeaturedJob" onClick={() => addFilter('Featured')}>
+              Featured
+            </span>
+          )}
+          <JobTitle className="JobTitle">{job.position}</JobTitle>
+
+          <div className="MetaInfoWrapper">
+            <span className="MetaInfo">{job.postedAt}</span>
+            <div className="MetaInfoDot"></div>
+            <span className="MetaInfo">{job.contract}</span>
+            <div className="MetaInfoDot"></div>
+            <span className="MetaInfo">{job.location}</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="Column">
+        {skills.map((skill, index) => (
+          <Skill
+            className="Skill"
+            key={index}
+            isAlreadySelected={filters.some(
+              (filter) => filter.text === skills[index],
+            )}
+            onClick={() => {
+              addFilter(skills[index]);
+            }}
+          >
+            {skill}
+          </Skill>
+        ))}
+      </div>
+    </Wrapper>
+  );
 };
