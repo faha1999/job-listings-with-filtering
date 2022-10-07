4978d4a785fb096b8582f26b31a947e6342f2431
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-10-07 22:24:04 +0600

    added job card styles

--- src/components/jobcard/JobCard.jsx
+++ src/components/jobcard/JobCard.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  background: ${({ theme }) => theme.cardBg};
+  box-shadow: ${({ theme }) =>
+    theme === 'light' ? 'var(--box-shadow)' : 'none'};
+  border-left: ${({ featured }) =>
+    featured ? '0.35em solid var(--desaturated-dark-cyan)' : null};
+`;
+
+const JobTitle = styled.h3`
+  color: ${({ theme }) => theme.text};
+`;
+
+const Skill = styled.button`
+  color: ${({ theme }) => theme.pillText};
+  background: ${({ theme }) => theme.pillBg};
+
+  :hover {
+    background: ${({ theme }) => theme.pillBgDark};
+  }
+`;
 export const JobCard = () => {
   return <div>JobCard</div>;
 };
