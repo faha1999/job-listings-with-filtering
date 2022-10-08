8735024e882a3e7063e1f84c37bdf1e86e1b4262
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-10-08 14:33:24 +0600

    applied FilterBox styles

--- src/components/FilterBox/FilterBox.jsx
+++ src/components/FilterBox/FilterBox.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FilterBoxWrapper = styled.div`
+  background: ${({ theme }) => theme.cardBg};
+  box-shadow: ${({ theme }) => (theme === 'light' ? '$box-shadow' : 'none')};
+`;
+
+const FilterInput = styled.input`
+  background: ${({ theme }) => theme.cardBg};
+`;
 
 export const FilterBox = () => {
   return <div>FilterBox</div>;
