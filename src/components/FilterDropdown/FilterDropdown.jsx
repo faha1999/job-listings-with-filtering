14a8a82d3846165754490bc50497745c15f59154
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-10-09 13:39:50 +0600

    fixed dropdown issue

--- src/components/FilterDropdown/FilterDropdown.jsx
+++ src/components/FilterDropdown/FilterDropdown.jsx
@@ -1,18 +1,16 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { Context } from '../../Context';
-import ItachiTrollFace from '../../assets/images/itachi-troll-face.png';
+import ItachiTrollFace from '../../assets/images/sad-face.png';
 
 const FilterDropdownWrapper = styled.div`
   background: ${({ theme }) => theme.cardBg};
-  box-shadow: ${({ theme }) =>
-    theme === 'light' ? 'var(--box-shadow)' : 'none'};
 `;
 
 export const FilterDropdown = ({ setValue, dropdownTexts }) => {
   const { addFilter } = useContext(Context);
   return (
-    <FilterDropdownWrapper>
+    <FilterDropdownWrapper className="FilterDropdownWrapper">
       {dropdownTexts.length > 0 ? (
         dropdownTexts.map((text, index) => (
           <p
